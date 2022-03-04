2e54fe5bda1f4fb8fb79c81a7ed989c0c83184ea
M. Stoltzfus <contact@mstoltzfus.com>
2022-03-04 11:00:21 -0500

    webhooktester

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,55 @@
 import React from 'react';
-import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Stack, IStackTokens, IStackStyles } from '@fluentui/react';
+import { TextField } from '@fluentui/react/lib/TextField';
 import './App.css';
 
-const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
 const stackTokens: IStackTokens = { childrenGap: 15 };
 const stackStyles: Partial<IStackStyles> = {
   root: {
-    width: '960px',
+    width: '250px',
     margin: '0 auto',
     textAlign: 'center',
-    color: '#605e5c',
-  },
+    color: '#605e5c'
+    },
 };
 
 export const App: React.FunctionComponent = () => {
+
+  const [callerNumberState, setCallerNumberState] = useState( '' );
+  const [displayNameState, setDisplayNameState] = useState( '' );
+
+  const url = new URL( document.URL );
+  const urlSp = url.searchParams;
+
+  let callerNumber = urlSp.get( 'callernumber' );
+  let displayName = urlSp.get( 'displayname' );
+
+  useEffect( () => {
+    urlSearchParamsHandler();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [] );
+
+  const urlSearchParamsHandler = () => {
+    console.log( 'URL params: number - ' + callerNumber + ' name - ' + displayName );
+
+    if ( callerNumber == null || displayName == null ) {
+      setCallerNumberState( 'No caller number' );
+      setDisplayNameState( 'No display name' );
+    } else {
+      setCallerNumberState( callerNumber );
+      setDisplayNameState( displayName );
+    }
+  };
+
+
   return (
+    <div className="App">
     <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-      <img className="App-logo" src={logo} alt="logo" />
-      <Text variant="xxLarge" styles={boldStyle}>
-        Welcome to your Fluent UI app
-      </Text>
-      <Text variant="large">For a guide on how to customize this project, check out the Fluent UI documentation.</Text>
-      <Text variant="large" styles={boldStyle}>
-        Essential links
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-        <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-        <Link href="https://twitter.com/fluentui">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design system
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">Icons</Link>
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">Styles</Link>
-        <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-      </Stack>
+      <h2> The URL Params are - </h2>
+      <TextField label="Caller Number" readOnly value={callerNumberState} />
+      <TextField label="Display Name" readOnly value={displayNameState} />
     </Stack>
+    </div>
   );
 };
