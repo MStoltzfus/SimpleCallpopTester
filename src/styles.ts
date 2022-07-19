6b2d94ecea55a95beea0b662cee42fd06a1f1470
M. Stoltzfus <contact@mstoltzfus.com>
2022-07-19 12:17:55 -0400

    Added Theming for Toggle-able Darkmode

---
+++ src/styles.ts
@@ -0,0 +1,12 @@
+import { IStackStyles, IStackTokens, ITextStyles } from "@fluentui/react";
+
+
+export const stackTokens: IStackTokens = { childrenGap: 15 };
+export const stackStyles: Partial<IStackStyles> = {
+  root: {
+    width: '250px',
+    margin: '0 auto',
+    textAlign: 'center',
+    color: '#605e5c'
+  },
+};
