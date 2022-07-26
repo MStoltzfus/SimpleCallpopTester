abafb8e80f19a8d7189c1f4a6c14699dd9bf7d80
M Stoltzfus <contact@mstoltzfus.com>
2022-07-25 22:25:25 -0400

    Added a new text element | fixed logic

--- src/app/components/ListItemsComponent.tsx
+++ src/app/components/ListItemsComponent.tsx
@@ -9,16 +9,18 @@ const NoParamsMessage = () => {
       <>
         <Text>This app generates components based on URL search params specified in the URL</Text>
         <Text>To see it in action, add some params to the URL in the "example.com?key=value" format</Text>
+        <br/>
+        <Text variant={'small'}>Psst - you can now set the app heading to a custom title using the "appName" parameter</Text>
       </>
     </>
   );
 }
 
 const ListItemsComponent = ( props: any ) => {
-  let excludeAppNameKey = props.props.filter((data: { key: string; }) => data.key !== 'appName'); //Dirty fix that excludes the appName URL param/value from being rendered in the list
   if ( props.props === undefined ) {
     return <NoParamsMessage />
   } else {
+    let excludeAppNameKey = props.props.filter((data: { key: string; }) => data.key !== 'appName'); //Dirty fix that excludes the appName URL param/value from being rendered in the list
     return excludeAppNameKey.map( ( props: any ) => ( <TextField key={props.key} label={props.key} readOnly value={props.value} /> ) );
   }
 };
