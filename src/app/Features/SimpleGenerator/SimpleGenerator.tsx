f2bf6ec655c6eb309e905cf6987ae9ae1e48ba94
M. Stoltzfus <contact@mstoltzfus.com>
2023-03-07 16:19:21 -0500

    init for notes component

--- src/app/Features/SimpleGenerator/SimpleGenerator.tsx
+++ src/app/Features/SimpleGenerator/SimpleGenerator.tsx
@@ -2,11 +2,12 @@ import { Stack } from '@fluentui/react';
 import ListItemsComponent from './pageComponents/ListItemsComponent';
 import { stackStyles, stackTokens, mainContentStyle } from '../../styles'; // import the styles from the styles file
 import utils from '../../utils';
+import { useEffect, useState } from 'react';
   
   const SimpleGenerator = () => {
 
     let searchParams = utils.getAllUrlParams(); //Calls the method to get URL Search Params and create an easy to work with array from them
-
+    
     return <div style={mainContentStyle}>
       <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
         <ListItemsComponent props={searchParams} />
